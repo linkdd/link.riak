dec5632e3ddd34491dc44fd308e8f5cecf5b16fe
David Delassus <david.jose.delassus@gmail.com>
2016-07-12 15:12:27 +0200

    :add: ArrayField handling in model feature

--- link/riak/features/model.py
+++ link/riak/features/model.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from b3j0f.utils.iterable import isiterable
+from b3j0f.aop import weave, get_advices
 from b3j0f.conf import Configurable
+
 from xml.etree import ElementTree as ET
 from six import string_types
 from copy import deepcopy
@@ -11,6 +14,14 @@ from link.riak import CONF_BASE_PATH
 
 class solr:
     class BaseField(property):
+        @property
+        def name(self):
+            return self._attr['name']
+
+        @property
+        def attr(self):
+            return '_{0}'.format(self._attr['name'])
+
         def __init__(self, attributes, *args, **kwargs):
             kwargs['fget'] = self._fget
             kwargs['fset'] = self._fset
@@ -19,46 +30,119 @@ class solr:
             super(solr.BaseField, self).__init__(*args, **kwargs)
 
             self._attr = attributes
-            self._value = None
 
-        def _fget(self):
+        def convert_value(self, val):
             raise NotImplementedError()
 
-        def _fset(self):
+        def setdefault(self, obj):
             raise NotImplementedError()
 
-        def _fdel(self):
-            raise NotImplementedError()
+        def _fget(self, obj):
+            try:
+                result = getattr(obj, self.attr)
+
+            except AttributeError:
+                result = self.setdefault(obj)
+
+            return result
+
+        def _fset(self, obj, val):
+            val = self.convert_value(val)
+            setattr(obj, self.attr, val)
+
+        def _fdel(self, obj):
+            delattr(obj, self.attr)
 
     class TrieIntField(BaseField):
-        def _fget(self):
-            return self._value
-
-        def _fset(self, val):
+        def convert_value(self, val):
             if not isinstance(val, int):
                 val = int(val)
 
-            self._value = val
+            return val
+
+        def setdefault(self, obj):
+            result = 0
+            setattr(obj, self.attr, result)
+            return result
 
     class BoolField(BaseField):
-        def _fget(self):
-            return self._value
-
-        def _fset(self, val):
+        def convert_value(self, val):
             if not isinstance(val, bool):
                 val = (val == 'true')
 
-            self._value = val
+            return val
+
+        def setdefault(self, obj):
+            result = False
+            setattr(obj, self.attr, result)
+            return result
 
     class StrField(BaseField):
-        def _fget(self):
-            return self._value
-
-        def _fset(self, val):
+        def convert_value(self, val):
             if not isinstance(val, string_types):
                 val = str(val)
 
-            self._value = val
+            return val
+
+        def setdefault(self, obj):
+            result = ''
+            setattr(obj, self.attr, result)
+            return result
+
+    class ArrayField(BaseField):
+        def __init__(self, subtype, *args, **kwargs):
+            super(solr.ArrayField, self).__init__(*args, **kwargs)
+
+            self.subtype = subtype(self.attr)
+
+        def ensure_type_append(self, jointpoint):
+            args = list(jointpoint.args)
+            args[0] = self.subtype.convert_value(args[0])
+            jointpoint.args = tuple(args)
+
+            return jointpoint.proceed()
+
+        def ensure_type_extend(self, jointpoint):
+            L = jointpoint.args[0]
+
+            for i, item in enumerate(L):
+                L[i] = self.subtype.convert_value(item)
+
+            return jointpoint.proceed()
+
+        def ensure_type_insert(self, jointpoint):
+            args = list(jointpoint)
+            args[1] = self.subtype.convert_value(args[1])
+            jointpoint.args = tuple(args)
+
+            return jointpoint.proceed()
+
+        def weave_list(self, val):
+            methods = [
+                (val.append, self.ensure_type_append),
+                (val.extend, self.ensure_type_extend),
+                (val.insert, self.ensure_type_insert),
+                (val.remove, self.ensure_type_append),
+                (val.__setitem__, self.ensure_type_insert)
+            ]
+
+            for method, advice in methods:
+                if advice not in get_advices(method):
+                    weave(target=method, advices=advice)
+
+        def setdefault(self, obj):
+            result = []
+            self.weave_list(result)
+            setattr(obj, self.attr, result)
+            return result
+
+        def convert_value(self, val):
+            if not isiterable(val, exclude=string_types):
+                val = [val]
+
+            self.weave_list(val)
+
+            return val
 
 
 @Configurable(paths='{0}/model.conf'.format(CONF_BASE_PATH))
@@ -91,6 +175,12 @@ class RiakSolrSchema(ModelFeature):
             fattr = deepcopy(field.attrib)
             fattr.update(typeattr)
 
-            clsmembers[fname] = ftype(fattr)
+            isarray = (field.get('multiValued', 'false') == 'true')
+
+            if isarray:
+                clsmembers[fname] = solr.ArrayField(ftype, attr)
+
+            else:
+                clsmembers[fname] = ftype(fattr)
 
         return type(clsname, clsbases, clsmembers)
